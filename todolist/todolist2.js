5667e49a2cc675519792eafb387139810aec03d4
Gyeongho Jeon <70951270+Jeongyeongho93@users.noreply.github.com>
2021-09-29 02:28:46 +0900

    Create todolist2.js

---
+++ todolist/todolist2.js
@@ -0,0 +1,39 @@
+//How to paint, save and convert the array to string as below
+
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("#todo-form input");
+const toDoList = document.getElementById("todo-list");
+
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
+function deleteTodo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+function paintToDo(newTodo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteTodo)
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+    event.preventDefault();
+    const newTodo = toDoInput.value;
+    toDoInput.value = "";
+    toDos.push(newTodo);
+    paintToDo(newTodo);
+    saveToDos();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
